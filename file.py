04016e37bd2fdd1fda59a36708acd6436e92a248
Yuvraj K <Yuvrajbhagyedh@gmail.com>
2026-04-18 10:59:23 +0530

    message

---
+++ file.py
@@ -0,0 +1,9 @@
+f=open("C:/Users/HP\Downloads/basic programs.txt","r+")
+data=f.read()
+
+stri="24MCAL17.1"
+mess="hello"
+if data == stri:
+    f=f=open("C:/Users/HP\Downloads/basic programs.txt","w")
+    stri=mess
+print(data)

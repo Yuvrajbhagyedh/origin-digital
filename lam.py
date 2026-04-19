ca5cff4084bfdd6eebdd16547f15e154f6d3f951
Yuvraj K <Yuvrajbhagyedh@gmail.com>
2026-04-19 12:20:07 +0530

    map reduce filter

--- lam.py
+++ lam.py
@@ -2,7 +2,7 @@ from functools import reduce
 
 a=[12,7,65,4,645,3]
 res=list(filter(lambda a:a%2==0,a))
-ma=list(map(lambda a:a+2,res))
+ma=list(map(lambda a:a*82,res))
 rs=reduce(lambda b,a:b+a,res)
 
 print(ma)
